4308eebe5d6e2493e3b0842541dc24060cb868c1
TheRealSleeper <140853156+TheRealSleeper@users.noreply.github.com>
2025-12-01 11:04:30 -0600

    Initial commit

---
+++ src/tests.rs
@@ -0,0 +1,13 @@
+use super::*;
+
+fn test(func: fn(&str) -> AnswerType, answer: AnswerType, path: &str) {
+    assert_eq!(
+        func(&std::fs::read_to_string(path).expect("Unable to read file")),
+        answer
+    );
+}
+
+#[test]
+fn test1() {
+    test(super::part1, todo!(), "input/sample1.txt");
+}
