43daa67c4e81c1f8da6237ad8c77461a1fb8af22
TheRealSleeper <140853156+TheRealSleeper@users.noreply.github.com>
2025-12-03 11:02:07 -0600

    Both parts solved

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::fs::read_to_string;
 
 #[allow(dead_code)]
@@ -6,7 +7,7 @@ mod aoc_lib;
 #[cfg(test)]
 mod tests;
 
-type AnswerType = usize;
+type AnswerType = u64;
 
 fn main() {
     let args = aoc_lib::Args::get();
@@ -35,9 +36,63 @@ fn main() {
 }
 
 fn part1(_input: &str) -> AnswerType {
-    todo!()
+    use std::cmp::Ordering::*;
+    _input
+        .lines()
+        .map(|l| {
+            let batteries = l
+                .trim()
+                .chars()
+                .map(|c| c.to_digit(10).unwrap() as u64)
+                .collect_vec();
+            let mut total = 0;
+            let mut max_pos = 0;
+            for i in (0..2).rev() {
+                let (rel_max_pos, max) = batteries[max_pos..batteries.len() - i]
+                    .iter()
+                    .enumerate()
+                    .max_by(|a, b| match a.1.cmp(b.1) {
+                        Less | Equal => Less,
+                        Greater => Greater,
+                    })
+                    .unwrap();
+                total *= 10;
+                total += max;
+                max_pos += rel_max_pos + 1;
+            }
+
+            total
+        })
+        .sum()
 }
 
 fn part2(_input: &str) -> AnswerType {
-    todo!()
+    use std::cmp::Ordering::*;
+    _input
+        .lines()
+        .map(|l| {
+            let batteries = l
+                .trim()
+                .chars()
+                .map(|c| c.to_digit(10).unwrap() as u64)
+                .collect_vec();
+            let mut total = 0;
+            let mut max_pos = 0;
+            for i in (0..12).rev() {
+                let (rel_max_pos, max) = batteries[max_pos..batteries.len() - i]
+                    .iter()
+                    .enumerate()
+                    .max_by(|a, b| match a.1.cmp(b.1) {
+                        Less | Equal => Less,
+                        Greater => Greater,
+                    })
+                    .unwrap();
+                total *= 10;
+                total += max;
+                max_pos += rel_max_pos + 1;
+            }
+
+            total
+        })
+        .sum()
 }
