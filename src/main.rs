587b6df589d855c6e6c03e17289aeeb53877b91e
TheRealSleeper <asleeper135@gmail.com>
2025-12-07 00:06:27 -0600

    Part 1 solved

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::fs::read_to_string;
 
 #[allow(dead_code)]
@@ -35,7 +36,59 @@ fn main() {
 }
 
 fn part1(_input: &str) -> AnswerType {
-    todo!()
+    let grid = _input
+        .lines()
+        .map(|l| l.bytes().collect_vec())
+        .collect_vec();
+
+    let mut beams = vec![false; grid[0].len()];
+    beams[grid[0].iter().position(|&b| b == b'S').unwrap()] = true;
+    let mut total = 0;
+
+    for row in &grid {
+        let mut new_beams = vec![];
+        let mut remove_beams = vec![];
+        for (pos, is_beam) in beams.iter().enumerate() {
+            if !is_beam {
+                continue;
+            }
+            let mut split = false;
+            if row[pos] == b'^' {
+                remove_beams.push(pos);
+                if pos > 0 {
+                    new_beams.push(pos - 1);
+                    total += 1;
+                    split = true;
+                }
+                if pos < row.len() - 1 {
+                    new_beams.push(pos + 1);
+                    if !split {
+                        total += 1;
+                    }
+                }
+            }
+        }
+
+        for &beam in &new_beams {
+            beams[beam] = true;
+        }
+        new_beams.clear();
+
+        for &beam in &remove_beams {
+            beams[beam] = false;
+        }
+        remove_beams.clear();
+        for i in 0..row.len() {
+            if beams[i] {
+                print!("|");
+            } else {
+                print!("{}", row[i] as char);
+            }
+        }
+        println!();
+    }
+
+    total
 }
 
 fn part2(_input: &str) -> AnswerType {
