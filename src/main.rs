b5aad197d8f2cdf482fbd7bcd48fa906e055c7f7
TheRealSleeper <asleeper135@gmail.com>
2025-12-08 00:00:22 -0600

    Part 1 WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::fs::read_to_string;
 
 #[allow(dead_code)]
@@ -35,7 +36,53 @@ fn main() {
 }
 
 fn part1(_input: &str) -> AnswerType {
-    todo!()
+    let jbs = _input
+        .lines()
+        .map(|l| {
+            let mut coords = l.split(',');
+            (
+                coords.next().unwrap().parse::<f64>().unwrap(),
+                coords.next().unwrap().parse::<f64>().unwrap(),
+                coords.next().unwrap().parse::<f64>().unwrap(),
+            )
+        })
+        .collect_vec();
+
+    let combos = jbs
+        .iter()
+        .enumerate()
+        .combinations(2)
+        .map(|combo| {
+            let distance = ((combo[0].1.0 - combo[1].1.0).powi(2)
+                + (combo[0].1.1 - combo[1].1.1).powi(2)
+                + (combo[0].1.2 - combo[1].1.2).powi(2))
+            .sqrt();
+            (combo[0], combo[1], distance)
+        })
+        .sorted_unstable_by(|a, b| a.2.total_cmp(&b.2))
+        .collect_vec();
+
+    let mut circuits = vec![vec![]];
+    'outer: for combo in &combos {
+        for circuit in &mut circuits {
+            if circuit.contains(&&combo.0) && circuit.contains(&&combo.1) {
+                continue 'outer;
+            }
+            if circuit.contains(&&combo.0) && !circuit.contains(&&combo.1) {
+                circuit.push(&combo.1);
+                continue 'outer;
+            } else if !circuit.contains(&&combo.0) && circuit.contains(&&combo.1) {
+                circuit.push(&combo.0);
+                continue 'outer;
+            }
+        }
+        circuits.push(vec![&combo.0, &combo.1]);
+    }
+
+    circuits.sort_unstable_by_key(|c| c.len());
+    circuits.reverse();
+
+    circuits[0].len() * circuits[1].len() * circuits[2].len()
 }
 
 fn part2(_input: &str) -> AnswerType {
