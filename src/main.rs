c7e99ad125dcee5bf6d7b55b3d6662b3fe33c195
TheRealSleeper <asleeper135@gmail.com>
2026-09-25 16:01:57 -0500

    Both parts solved

--- src/main.rs
+++ src/main.rs
@@ -44,23 +44,66 @@ fn parse_input(input: &str) -> HashMap<&str, Vec<&str>> {
         .collect()
 }
 
-fn count_paths(node: &str, map: &HashMap<&str, Vec<&str>>, count: &mut AnswerType ) {
-    let devs = &map[node]; 
-    if devs.contains(&"out") {
-        *count += 1; 
+fn count_paths<'a>(
+    node: &'a str,
+    end: &str,
+    map: &HashMap<&str, Vec<&'a str>>,
+    count: &mut AnswerType,
+    cache: &mut HashMap<&'a str, AnswerType>,
+) {
+    if !map.contains_key(node) {
+        return;
+    }
+
+    if cache.contains_key(node) {
+        *count += cache[node];
+        return;
+    }
+
+    let devs = &map[node];
+    if devs.contains(&end) {
+        *count += 1;
+        cache.insert(node, 1);
     } else {
+        let mut new_count = 0;
         for dev in devs {
-            count_paths(dev, map, count);
+            count_paths(dev, end, map, &mut new_count, cache);
         }
+        cache.insert(node, new_count);
+        *count += new_count;
     }
 }
 
 fn part1(_input: &str) -> AnswerType {
-    let mut count = 0; 
-    count_paths("you", &parse_input(_input), &mut count);
+    let mut count = 0;
+    count_paths(
+        "you",
+        "out",
+        &parse_input(_input),
+        &mut count,
+        &mut HashMap::new(),
+    );
     count
 }
 
 fn part2(_input: &str) -> AnswerType {
-    todo!()
+    let map = parse_input(_input);
+
+    let mut dac_paths = 0;
+    count_paths("dac", "fft", &map, &mut dac_paths, &mut HashMap::new());
+
+    let mut fft_paths = 0;
+    count_paths("fft", "dac", &map, &mut fft_paths, &mut HashMap::new());
+
+    let first = if dac_paths == 0 { "fft" } else { "dac" };
+    let second = if dac_paths == 0 { "dac" } else { "fft" };
+    let mid_paths = if dac_paths == 0 { fft_paths } else { dac_paths };
+
+    let mut start_paths = 0;
+    count_paths("svr", first, &map, &mut start_paths, &mut HashMap::new());
+
+    let mut end_paths = 0;
+    count_paths(second, "out", &map, &mut end_paths, &mut HashMap::new());
+
+    start_paths * mid_paths * end_paths
 }
