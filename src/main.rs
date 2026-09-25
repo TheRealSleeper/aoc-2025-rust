a4d033e18d4b8dbfe1295286e66c9c242271c352
TheRealSleeper <asleeper135@gmail.com>
2026-09-25 14:04:41 -0500

    Part 1 Solved

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::read_to_string;
+use std::{collections::HashMap, fs::read_to_string};
 
 #[allow(dead_code)]
 mod aoc_lib;
@@ -34,8 +34,31 @@ fn main() {
     }
 }
 
+fn parse_input(input: &str) -> HashMap<&str, Vec<&str>> {
+    input
+        .lines()
+        .map(|l| {
+            let (dev, conns) = l.split_once(':').unwrap();
+            (dev, conns.split_whitespace().collect())
+        })
+        .collect()
+}
+
+fn count_paths(node: &str, map: &HashMap<&str, Vec<&str>>, count: &mut AnswerType ) {
+    let devs = &map[node]; 
+    if devs.contains(&"out") {
+        *count += 1; 
+    } else {
+        for dev in devs {
+            count_paths(dev, map, count);
+        }
+    }
+}
+
 fn part1(_input: &str) -> AnswerType {
-    todo!()
+    let mut count = 0; 
+    count_paths("you", &parse_input(_input), &mut count);
+    count
 }
 
 fn part2(_input: &str) -> AnswerType {
